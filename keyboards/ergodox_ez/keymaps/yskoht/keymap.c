8437904faa32340aa0b802718693bb7bd2d8c82f
yskoht <ysk.oht@gmail.com>
2018-02-18 15:01:09 +0900

    modified ergodox_ez/keymaps/yskoht

--- keyboards/ergodox_ez/keymaps/yskoht/keymap.c
+++ keyboards/ergodox_ez/keymaps/yskoht/keymap.c
@@ -18,19 +18,19 @@ enum tap_dance_codes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(
         KC_ESC,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,
-        KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,
+        KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    LCTL(KC_Z),
         TD(TD_Ctrl_CtrlG), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,
-        KC_LSFT,           KC_LGUI, KC_LALT, KC_LALT, LT(LOWER,XXXXXXX),
+        KC_LCTRL,          KC_LGUI, KC_LALT, KC_LALT, LT(LOWER,XXXXXXX),
                                                             LCTL(KC_C), LCTL(KC_V),
                                                                         XXXXXXX,
-                                                      KC_LSFT, XXXXXXX, XXXXXXX,
+                                                      KC_LSFT,  KC_SPC, KC_ENT,
 
-        XXXXXXX,  KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        XXXXXXX,  KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-                  KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-               LT(RAISE, XXXXXXX),   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
+        KC_PSCREEN, KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        LCTL(KC_Y), KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+                    KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_B,       KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+                LT(RAISE, XXXXXXX),   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
         KC_LEFT, KC_RIGHT,
         KC_UP,
         KC_DOWN, KC_ENT,   KC_SPC
