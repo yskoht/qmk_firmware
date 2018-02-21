65b0b0d2bde7d74ed811013b95a3a7aa3446a1bf
yskoht <ysk.oht@gmail.com>
2018-02-21 19:00:42 +0900

    modified ergodox_ez/keymaps/yskoht

--- keyboards/ergodox_ez/keymaps/yskoht/keymap.c
+++ keyboards/ergodox_ez/keymaps/yskoht/keymap.c
@@ -18,16 +18,16 @@ enum tap_dance_codes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(
         KC_ESC,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,
-        KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    CTL(KC_Z),
+        KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    LCTL(KC_Z),
         TD(TD_Ctrl_CtrlG), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,
         KC_LCTRL,          KC_LGUI, KC_LALT, KC_LALT, MO(LOWER),
-                                                             CTL(KC_C), CTL(KC_V),
-                                                                        ALT(KC_TAB),
-                                                   KC_LSFT, KC_SPC,     KC_ENT,
+                                                            LCTL(KC_C), LCTL(KC_V),
+                                                                        LALT(KC_TAB),
+                                                   KC_LSFT, KC_SPC,     KC_LALT,
 
         KC_PSCREEN, KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        CTL(KC_Y),  KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        LCTL(KC_Y), KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
                     KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_B,       KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
                     MO(RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                              _______,
                                            _______, _______, _______,
 
-       _______, _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______, KC_DEL,
        _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
                 KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
        _______, KC_F12,  _______, _______, _______, _______, _______,
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     _______,
                                   _______, _______, _______,
 
-       _______,  _______, _______, _______, _______, _______, _______,
+       _______,  _______, _______, _______, _______, _______, KC_DEL,
        _______,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
                  KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
        _______,  KC_F12,  _______, _______, _______, _______, _______,
