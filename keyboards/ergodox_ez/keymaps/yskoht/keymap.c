0ffebeec2c9294c74b4fcd3bd558fdca9dc6b9ad
yskoht <ysk.oht@gmail.com>
2018-03-20 00:32:10 +0900

    modify ergodox_ez/keymaps/yskoht/keymap.c

--- keyboards/ergodox_ez/keymaps/yskoht/keymap.c
+++ keyboards/ergodox_ez/keymaps/yskoht/keymap.c
@@ -20,16 +20,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* .---------------------------------------------. .---------------------------------------------.
  * |  ESC  |  1  |  2  |  3  |  4  |  5  |       | !  PSc  |  6  |  7  |  8  |  9  |  0  | BSPC  |
  * !-------+-----+-----+-----+-----+-------------! !-------+-----+-----+-----+-----+-----+-------!
- * |  TAB  |  Q  |  W  |  E  |  R  |  T  | UNDO  | ! REDO  |  Y  |  U  |  I  |  O  |  P  | BSPC  |
+ * |  TAB  |  Q  |  W  |  E  |  R  |  T  | CtrlG | ! PASTE |  Y  |  U  |  I  |  O  |  P  | BSPC  |
  * !-------+-----+-----+-----x-----x-----!       ! !       !-----x-----x-----+-----+-----+-------!
- * |Ctr/Cg |  A  |  S  |  D  |  F  |  G  |-------! !-------!  H  |  J  |  K  |  L  |SCLN | QUOT  |
+ * | LCTRL |  A  |  S  |  D  |  F  |  G  |-------! !-------!  H  |  J  |  K  |  L  |SCLN | QUOT  |
  * !-------+-----+-----+-----x-----x-----!   N   ! !   B   !-----x-----x-----+-----+-----+-------!
  * | LSFT  |  Z  |  X  |  C  |  V  |  B  |       | !       |  N  |  M  |COMM | DOT |SLSH |ET/SFT |
  * '-------+-----+-----+-----+-----+-------------' '-------------+-----+-----+-----+-----+-------'
  *  |LCTRL |LGUI |LALT |LALT |LOWER|                             !RAISE|LEFT |DOWN | UP  |RIGHT |
  *  '------------------------------'                             '------------------------------'
  *                               .---------------. .---------------.
- *                               | COPY  | PASTE | ! LEFT  | RIGHT |
+ *                               | COPY  |SFT-SPC| ! LEFT  | RIGHT |
  *                       .-------+-------+-------! !-------+-------+-------.
  *                       ! LSFT  !  SPC  |ALT-TAB| !  UP   |  ENT  !  SPC  !
  *                       !       !       !-------! !-------!       !       !
@@ -38,16 +38,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox(
         KC_ESC,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,
-        KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    LCTL(KC_Z),
-        TD(TD_Ctrl_CtrlG), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+        KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    LCTL(KC_G),
+        KC_LCTRL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,
         KC_LCTRL,          KC_LGUI, KC_LALT, KC_LALT, MO(LOWER),
-                                                            LCTL(KC_C), LCTL(KC_V),
+                                                            LCTL(KC_C), LSFT(KC_SPACE),
                                                                         LALT(KC_TAB),
                                                    KC_LSFT, KC_SPC,     KC_LALT,
 
         KC_PSCREEN, KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        LCTL(KC_Y), KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        LCTL(KC_V), KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
                     KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_B,       KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
                     MO(RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
